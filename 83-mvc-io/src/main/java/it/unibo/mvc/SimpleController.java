af2fd1557d622642d8fe696577652af8930742a2
Emauele <emanuele.dajko@studio.unibo.it>
2022-11-18 17:13:47 +0100

    remove message "todo"

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,30 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void set_next_string_print(String printNextString) {
+        
+    }
+
+    @Override
+    public String get_next_string_print() {
+        return null;
+    }
+
+    @Override
+    public List<String> get_history_printed_string() {
+        return null;
+    }
+
+    @Override
+    public void print_current_string() {
+    }
 
 }
